6dcd4ebea72690422256187d727b94ca27c698f8
ptresky <peter_tresky@hotmail.com>
2024-07-30 21:07:31 -0700

    step 4 pass

--- js/app.js
+++ js/app.js
@@ -17,12 +17,42 @@ const init = () => {
     turn = 'X';
     winner = false;
     tie = false;
+    render()
 }
 init()
-const render = () => {
+function render() {
+    updateBoard();
+    updateMessage();
 }
 
-const updateBoard = () => {
+function updateBoard() {
     board.forEach((square, index) => {
-        square = squareEls[index]
-})}
+      if (square === 'X') {
+        squareEls[index].textContent = 'X'
+        } else if (square === 'O') {
+            squareEls[index].textContent = 'O'
+        } else {
+            squareEls[index].textContent = ''
+        }
+    })
+}
+
+function updateMessage () {
+    if (winner === false && tie === false) {
+        if (turn === 'X') {
+            messageEl.textContent = "It's X's turn"
+        } else {
+            messageEl.textContent = "it's O's turn"
+        }
+    } 
+    if (winner === false && tie === true) {
+        messageEl.textContent = "It's a tie!"
+    }
+    if (winner === true && tie === false) {
+        if (turn === 'X') {
+        messageEl.textContent = "Congratulations X! You Won!"
+    } else {
+        messageEl.textContent = "Congratulations O! You Won"
+    }
+}
+}
